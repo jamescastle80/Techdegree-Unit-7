2296b27813d3518c00fed9e23dcde39b446a7ecf
James Castle <jamescastle@Jamess-MacBook-Pro.local>
2021-04-26 15:44:21 +0200

    line graph installed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// alert banner
 
 const alertBanner = document.getElementById("alert");
 
@@ -15,3 +16,45 @@ alertBanner.innerHTML =
             alertBanner.style.display = "none";
         }
     });
+
+// line graph
+
+let trafficCanvas = document.getElementById("traffic-chart");
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [500, 1200, 1000, 2000, 1500, 1750, 1250, 1800, 2200, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+    }]
+};
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+});
+
+
+
+// bar graph
+
+// pie chart
