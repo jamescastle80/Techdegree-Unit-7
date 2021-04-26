fa7ee7a4454be49c94c8829d9b288050518f5f0b
James Castle <jamescastle@Jamess-MacBook-Pro.local>
2021-04-26 16:48:13 +0200

    charts added

--- app.js
+++ app.js
@@ -19,18 +19,20 @@ alertBanner.innerHTML =
 
 // line graph
 
-let trafficCanvas = document.getElementById("traffic-chart");
+const trafficCanvas = document.getElementById("traffic-chart");
 
-let trafficData = {
+const trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
+        fill: true,
+        tension: .5,
         data: [500, 1200, 1000, 2000, 1500, 1750, 1250, 1800, 2200, 1500, 2500],
         backgroundColor: 'rgba(116, 119, 191, .3)',
         borderWidth: 1,
     }]
 };
 
-let trafficOptions = {
+const trafficOptions = {
     aspectRatio: 2.5,
     animation: {
         duration: 0
@@ -55,6 +57,72 @@ let trafficChart = new Chart(trafficCanvas, {
 
 
 
+
+
 // bar graph
+const dailyCanvas = document.getElementById("daily-chart");
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 120, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1,
+    }]
+};
+
+const dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
 
 // pie chart
+
+const mobileCanvas = document.getElementById("mobile-chart");
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+}
+
+const mobileOptions = {
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold'
+            }
+        }
+    }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
