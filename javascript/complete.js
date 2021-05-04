5bbe9fd94b7797674c44c5071abbb4b3d3131e50
James Castle <jamescastle@Jamess-MacBook-Pro.local>
2021-05-04 07:05:19 +0200

    search bug fix

--- javascript/complete.js
+++ javascript/complete.js
@@ -49,17 +49,20 @@ createNames(names);
 
 function filterNames(event) {
     if (nameInput !== "") {
-    box.classList.remove('hide');
-    }
+        box.classList.remove('hide');
+    } 
     var searchvalue = event.target.value.toLowerCase();
     var filterNames = names.filter((v,i) => {
         return(v.name.includes(searchvalue));
     })
     createNames(filterNames);
+    if (nameLI.childElementCount == 10) {
+        box.classList.add('hide');
+    }
  }
 
  nameLI.addEventListener('click', (e) => {
-    console.log(e.target.textContent);
+   // console.log(e.target.textContent);
     nameInput.value = e.target.textContent;
     box.classList.add('hide');
  });
